f868fe8b8676b0f787fc0832895519e38f91acea
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2026-01-18 00:18:48 +1100

    feat: added basic workflow of a tcp server including broadcasting, managing connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/websocket"
+	"net"
+	"os"
+	// "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// }
+
+func read_server_msg(conn net.Conn, err error) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if err != nil {
+			break
+		}
+		fmt.Println(line)
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK) // Set the status code
-	fmt.Fprintln(w, "Hello, World!")
+func handleError(err error, msg string, conn net.Conn) {
+	if msg == "" {
+		msg = "something went wrong"
+	}
+	if conn != nil {
+		fmt.Fprintln(conn, msg, err)
+	}
+	fmt.Fprintln(os.Stderr, msg, err)
+}
+
+func send_msg(conn net.Conn, err error) {
+	if err != nil {
+		handleError(err, "", conn)
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		fmt.Fprintln(conn, input)
+	}
 }
 
 func main() {
-
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Something is wrong, but I don't know what, err: ", err)
+	}
+	go read_server_msg(conn, err)
+	send_msg(conn, err)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,121 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
+	"net"
+	"slices"
+	"sync"
 
-	"github.com/gorilla/websocket"
+	// "net/http"
+	"os"
+	// "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+type Client struct {
+	name       string
+	connection net.Conn
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK) // Set the status code
-	fmt.Fprintln(w, "Hello, World!")
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// }
+
+//	func handler(w http.ResponseWriter, r *http.Request) {
+//		w.WriteHeader(http.StatusOK) // Set the status code
+//		fmt.Fprintln(w, "Hello, World!")
+//		fmt.Println(r.RemoteAddr)
+//		fmt.Println(r.Header.Get("X-Forwarded-For"))
+//	}
+var (
+	clients  []Client
+	clientMu sync.Mutex
+)
+
+func handleError(err error, msg string, conn net.Conn) {
+	if msg == "" {
+		msg = "something went wrong"
+	}
+	if conn != nil {
+		fmt.Fprintln(conn, msg, err)
+	}
+	fmt.Fprintln(os.Stderr, msg, err)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Fprintln(conn, "200/OK")
+	var name string
+	fmt.Fprintln(conn, "Please put in your name")
+	scanner := bufio.NewScanner(conn)
+	if scanner.Scan() {
+		name = scanner.Text()
+		for name == "" {
+			fmt.Fprintln(conn, "Please put in your name, your name can't be empty")
+			if !scanner.Scan() {
+				return
+			}
+			name = scanner.Text()
+
+		}
+	}
+	defer handleDisconnect(conn)
+	fmt.Fprintf(conn, "Hello %s \n", name)
+	broadcast(name + " joined the chat")
+	newClient := Client{name, conn}
+	clientMu.Lock()
+	clients = append(clients, newClient)
+	clientMu.Unlock()
+	for scanner.Scan() {
+		msg := name + ": " + scanner.Text()
+		fmt.Fprintln(os.Stdout, msg)
+		broadcast(msg)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error with %s: %v\n", name, err)
+	}
+}
+
+func handleDisconnect(conn net.Conn) {
+	clientMu.Lock()
+	for index, client := range clients {
+		if client.connection == conn {
+			clients = slices.Delete(clients, index, index+1)
+			clientMu.Unlock()
+			broadcast(client.name + " has left the chat")
+			break
+		}
+	}
+}
+
+func broadcast(msg string) {
+	clientMu.Lock()
+	defer clientMu.Unlock()
+	for _, client := range clients {
+		_, err := fmt.Fprintln(client.connection, msg)
+		if err != nil {
+			fmt.Printf("Broadcast failed for %s, removing them.\n", client.name)
+		}
+	}
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	// http.HandleFunc("/", handler)
 	fmt.Println("server is serving...")
-	http.ListenAndServe(":8080", nil)
+	// http.ListenAndServe(":8080", nil)
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "something went wrong")
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			handleError(err, "", conn)
+		}
+		go handleConnection(conn)
+
+	}
 }
